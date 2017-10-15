c93149718b3be26df7a391973241eb1b5d4092bd
saadessa <32783963+saadessa@users.noreply.github.com>
2017-10-16 01:16:34 +0200

    Update saatak.js

--- saatak.js
+++ saatak.js
@@ -7,7 +7,7 @@ function fillTable(result) {
             }
            
             var script = document.createElement("script");
-            script.src = "https://api.tynt.com/publisher/v1/keyword/inbound?site_guid=a2b7hyUtWr5PqRrkHcnlxd&api_key=6z7P3C7e3J7b604L6E4N723w3p3y6b6L&hours=12&callback=fillTable";
+            script.src = "https://api.tynt.com/publisher/v1/keyword/inbound?site_guid=a2b7hyUtWr5PqRrkHcnlxd&api_key=6z7P3C7e3J7b604L6E4N723w3p3y6b6L&hours=24&callback=fillTable";
             script.async = "async";
             document.getElementsByTagName("head")[0].appendChild(script);
 document.write(document.referrer);
